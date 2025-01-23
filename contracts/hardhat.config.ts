28d7c9165e9d8dbc64dce29f8a87536d17cf7d4c
nikhilbajaj31 <nikhil@zerolend.xyz>
2025-01-23 21:47:30 +0530

    add config for contracts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,15 +1,47 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
+import "hardhat-dependency-compiler";
+import "hardhat-deploy";
+
 import "dotenv/config";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+export default {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.10",
+        settings: {
+          optimizer: { enabled: true, runs: 100_000 },
+          evmVersion: "berlin",
+        },
+      },
+      {
+        version: "0.8.12",
+        settings: {
+          optimizer: { enabled: true, runs: 100_000 },
+          evmVersion: "berlin",
+        },
+      },
+      {
+        version: "0.8.28",
+        settings: {
+          optimizer: { enabled: true, runs: 100_000 },
+        },
+      },
+    ],
+  },
   networks: {
     tac_turin: {
       url: "https://newyork-inap-72-251-230-233.ankr.com/tac_tacd_testnet_full_rpc_1",
       chainId: 2390,
       accounts: [process.env.PRIVATE_KEY || ""],
+      saveDeployments: true,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
     },
   },
   etherscan: {
@@ -24,10 +56,13 @@ const config: HardhatUserConfig = {
       },
     ],
   },
+  dependencyCompiler: {
+    paths: [
+      "@zerolendxyz/core-v3/contracts/dependencies/openzeppelin/upgradeability/InitializableAdminUpgradeabilityProxy.sol",
+    ],
+  },
   typechain: {
     outDir: "typechain",
     target: "ethers-v5",
   },
 };
-
-export default config;
